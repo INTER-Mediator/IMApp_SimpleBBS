45425fb44e8260088bd1148aa25d544e8b818fc5
msyk <nii@msyk.net>
2021-11-06 16:16:16 +0900

    Update for ver.9

--- index_context.php
+++ index_context.php
@@ -5,3 +5,98 @@
  * Date: 2018/05/26
  * Time: 18:59
  */
+require_once './vendor/inter-mediator/inter-mediator/INTER-Mediator.php';
+
+IM_Entry(array(
+    0 =>
+        array(
+            'name' => 'message',
+            'table' => 'chat',
+            'view' => 'chat',
+            'key' => 'id',
+            'query' =>
+                array(
+                    0 =>
+                        array(
+                            'field' => 'groupname',
+                            'value' => '',
+                            'operator' => 'IS NULL',
+                        ),
+                ),
+            'sort' =>
+                array(
+                    0 =>
+                        array(
+                            'field' => 'postdt',
+                            'direction' => 'desc',
+                        ),
+                ),
+            'post-reconstruct' => true,
+            'post-dismiss-message' => '投稿しました',
+            'validation' =>
+                array(
+                    0 =>
+                        array(
+                            'field' => 'user',
+                            'rule' => 'value != \'\'',
+                            'message' => '入力してください。',
+                        ),
+                    1 =>
+                        array(
+                            'field' => 'message',
+                            'rule' => 'value != \'\'',
+                            'message' => '入力してください。',
+                        ),
+                ),
+        ),
+    1 =>
+        array(
+            'name' => 'comment',
+            'table' => 'chat',
+            'view' => 'chat',
+            'key' => 'id',
+            'relation' =>
+                array(
+                    0 =>
+                        array(
+                            'foreign-key' => 'groupname',
+                            'join-field' => 'id',
+                            'operator' => '=',
+                        ),
+                ),
+            'post-reconstruct' => true,
+            'post-dismiss-message' => '投稿しました',
+            'sort' =>
+                array(
+                    0 =>
+                        array(
+                            'field' => 'postdt',
+                            'direction' => 'desc',
+                        ),
+                ),
+            'validation' =>
+                array(
+                    0 =>
+                        array(
+                            'field' => 'user',
+                            'rule' => 'value != \'\'',
+                            'message' => '入力してください。',
+                        ),
+                    1 =>
+                        array(
+                            'field' => 'message',
+                            'rule' => 'value != \'\'',
+                            'message' => '入力してください。',
+                        ),
+                ),
+        ),
+),
+    array(
+        'formatter' => array(
+            array('field' => 'chat@message', 'converter-class' => 'HTMLString'),
+        ),
+    ),
+    array(
+        'db-class' => 'PDO',
+    ),
+    false);
